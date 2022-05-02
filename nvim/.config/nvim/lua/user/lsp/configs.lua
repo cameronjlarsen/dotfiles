6e0eca13282f2ace23da28345476473773db7d2a
Cameron Larsen <cameronjlarsen@gmail.com>
2022-05-01 21:14:38 -0700

    fixed breaking changes from nvim 0.7 update

--- nvim/.config/nvim/lua/user/lsp/configs.lua
+++ nvim/.config/nvim/lua/user/lsp/configs.lua
@@ -16,9 +16,9 @@ for _, server in pairs(servers) do
             on_attach=require("user.lsp.handlers").on_attach,
             capabilities=require("user.lsp.handlers").capabilities,
 }
-    local has_custom_opts, server_custom_opts=pcal(require, "user.lsp.settings." .. server)
-    if has_custom_opts then
-          opts=vim.tbl_deep_extend("force", server_custom_opts, opts)
-    end
-    lspconfig[server].setup(opts)
+   -- local has_custom_opts, server_custom_opts=pcall(require, "user.lsp.settings." .. server)
+    --if has_custom_opts then
+      --    opts=vim.tbl_deep_extend("force", server_custom_opts, opts)
+    --end
+    --lspconfig[server].setup(opts)
 end
