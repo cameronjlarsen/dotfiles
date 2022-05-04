993e25eb1732ac47a151db87bf4e8184bc39bd17
Cameron Larsen <cameronjlarsen@gmail.com>
2022-05-03 21:38:43 -0700

    fixed issue with find files in whichkey

--- nvim/.config/nvim/lua/user/lsp/settings/sumneko_lua.lua
+++ nvim/.config/nvim/lua/user/lsp/settings/sumneko_lua.lua
@@ -1,15 +1,14 @@
 return {
 	settings = {
-
 		Lua = {
 			diagnostics = {
 				globals = { "vim" },
 			},
 			workspace = {
-				library = {
-					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-					[vim.fn.stdpath("config") .. "/lua"] = true,
-				},
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
+			telemetry = {
+				enable = false,
 			},
 		},
 	},
