f7051f959bb8f3b1ec8f6029aec05f1500a5fa3c
Cameron Larsen <camjlarsen@gmail.com>
2022-06-25 22:34:57 -0700

    <Fix> File restructure
    Added config directories under one common folder as they all follow that
    theme

--- kde-catppuccin/nvim/.config/nvim/lua/user/lsp/settings/omnisharp.lua
+++
@@ -1,5 +0,0 @@
-local pid = vim.fn.getpid()
-local omnisharp_bin = "/usr/bin/omnisharp"
-return {
-	cmd = { omnisharp_bin, "--languageserver", "--hostPID", tostring(pid) },
-}
--- kde-catppuccin/nvim/.config/nvim/lua/user/notify.lua
+++
@@ -1,5 +0,0 @@
----@diagnostic disable: redundant-parameter
-require("notify").setup()
-
-vim.notify = require("notify")
-
