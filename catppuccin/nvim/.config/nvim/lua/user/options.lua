3b91ee887f7a9369655f203bdcd514f7f5ff91e6
Cameron Larsen <camjlarsen@gmail.com>
2022-07-03 22:35:45 -0700

    <Update> nvim options
    added ftplugin dir to set lua filetype options

--- catppuccin/nvim/.config/nvim/lua/user/options.lua
+++ catppuccin/nvim/.config/nvim/lua/user/options.lua
@@ -17,7 +17,7 @@ vim.opt.splitbelow = true                       -- force all horizontal splits t
 vim.opt.splitright = true                       -- force all vertical splits to go to the right of current window
 vim.opt.swapfile = false                        -- creates a swapfile
 vim.opt.termguicolors = true                    -- set term gui colors (most terminals support this)
-vim.opt.timeoutlen = 500                       -- time to wait for a mapped sequence to complete (in milliseconds)
+vim.opt.timeoutlen = 500                        -- time to wait for a mapped sequence to complete (in milliseconds)
 vim.opt.undofile = true                         -- enable persistent undo
 vim.opt.updatetime = 300                        -- faster completion (4000ms default)
 vim.opt.writebackup = false                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
@@ -32,12 +32,11 @@ vim.opt.signcolumn = "yes"                      -- always show the sign column, 
 vim.opt.wrap = false                            -- display lines as one long line
 vim.opt.scrolloff = 8                           -- is one of my fav
 vim.opt.sidescrolloff = 8
-vim.opt.guifont = "hack:h16"               -- the font used in graphical neovim applications
+vim.opt.guifont = "hack:h16"                    -- the font used in graphical neovim applications
 -- vim.opt.shell  = "usr/bin/zsh"
 vim.opt.shortmess:append "c"
+vim.opt.whichwrap="<,>,[,],h,l"
 
-
-vim.cmd "set whichwrap+=<,>,[,],h,l"
 vim.cmd [[set iskeyword+=-]]
 vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to workl
 
