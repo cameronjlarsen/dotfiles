480c90e6555c1888fb40dbece039e811ef2838d9
Cameron Larsen <camjlarsen@gmail.com>
2022-09-28 17:11:14 -0700

    <Feat> Added geoclue support for redshift

--- .config/awesome/config/autorun.sh
+++ .config/awesome/config/autorun.sh
@@ -7,7 +7,8 @@ run() {
   fi
 }
 
+run /usr/lib/geoclue-2.0/demos/agent 
 run picom -b --experimental-backends --config ~/.config/picom/picom.conf
-run /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-run redshift-gtk &
-run redshift &
+run /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 
+run redshift-gtk 
+run redshift 
