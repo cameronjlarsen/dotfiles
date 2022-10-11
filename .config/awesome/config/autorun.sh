79fc654a31e663f28f6fd1a814a1e8cd52e3364f
Cameron Larsen <camjlarsen@gmail.com>
2022-10-10 22:30:42 -0700

    <Updated> Awesome autostart script
    Removed autostarting redshift as it is now a systemd service.

--- .config/awesome/config/autorun.sh
+++ .config/awesome/config/autorun.sh
@@ -11,4 +11,4 @@ run /usr/lib/geoclue-2.0/demos/agent
 run picom -b --experimental-backends --config ~/.config/picom/picom.conf
 run /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 
 run redshift-gtk 
-run redshift 
+# run redshift 
