9a96af9f9e111f7fbbaee31bee4147b507ffb148
Cameron Larsen <camjlarsen@gmail.com>
2022-11-22 20:23:03 -0800

    chore(nvim): added textwidth option to markdown filetype

--- .config/nvim/ftplugin/markdown.lua
+++ .config/nvim/ftplugin/markdown.lua
@@ -1,2 +1,3 @@
 vim.opt.shiftwidth = 4                          -- the number of spaces inserted for each indentation
 vim.opt.tabstop = 4                             -- insert 4 spaces for a tab
+vim.opt_local.textwidth = 120                   -- set textwidth 120
