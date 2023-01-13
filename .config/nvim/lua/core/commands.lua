0dad0fcf9e1e3c3a773e5a4b1bb2e1a9a2bd3086
Cameron Larsen <camjlarsen@gmail.com>
2023-01-12 23:13:16 -0800

    fix(nvim): Added command to load file marks

--- .config/nvim/lua/core/commands.lua
+++ .config/nvim/lua/core/commands.lua
@@ -74,3 +74,13 @@ autocmd("FileType", {
         vim.opt_local.cursorline = true
     end,
 })
+
+autocmd('BufReadPost', {
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
