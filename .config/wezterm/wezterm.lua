412e1b53a00e10a5a06b24e81eb799cccc032048
Cameron Larsen <camjlarsen@gmail.com>
2023-01-31 16:51:45 -0800

    chore(wezterm): Updated wezterm window padding

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -264,10 +264,10 @@ return {
 
     -- Window
     window_padding    = {
-        left   = 0,
-        right  = 0,
-        top    = 0,
-        bottom = 0,
+        left   = 8,
+        right  = 8,
+        top    = 8,
+        bottom = 8,
     },
     initial_cols      = 110,
     initial_rows      = 25,
