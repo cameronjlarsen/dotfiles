638093113622c0c31f7209c9d40d60cd134fc238
Cameron Larsen <camjlarsen@gmail.com>
2023-09-04 17:37:53 -0700

    feat(awesome): Open a calendar app on clock click

--- .config/awesome/widgets/clock.lua
+++ .config/awesome/widgets/clock.lua
@@ -1,3 +1,4 @@
+local awful = require "awful"
 local wibox = require("wibox")
 
 local clock = wibox.widget {
@@ -11,6 +12,19 @@ end)
 
 clock:connect_signal("mouse::leave", function()
     awesome.emit_signal("calendar::visibility", false)
+end)
+
+clock:connect_signal("button::press", function()
+    awful.spawn.raise_or_spawn("morgen", nil, function(c)
+        if c.class == "morgen" then
+            c:jump_to()
+            awesome.emit_signal("calendar::visibility", false)
+            return true
+        else
+            awesome.emit_signal("calendar::visibility", false)
+            return false
+        end
+    end)
 end)
 
 
