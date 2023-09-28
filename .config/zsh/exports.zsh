46f6f309fd382433c17a8e413bd84557464a890b
Cameron Larsen <camjlarsen@gmail.com>
2023-09-27 17:16:15 -0700

    chore(zsh): Added conda setup for linux

--- .config/zsh/exports.zsh
+++ .config/zsh/exports.zsh
@@ -47,10 +47,9 @@ export QT_QPA_PLATFORMTHEME=qt5ct
 
 export SDKMAN_DIR="$HOME/.sdkman"
 
-# conda for macos
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
 if [[ $(uname -s) == 'Darwin' ]]; then
-    # >>> conda initialize >>>
-    # !! Contents within this block are managed by 'conda init' !!
     __conda_setup="$('/opt/homebrew/Caskroom/miniconda/base/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
     if [ $? -eq 0 ]; then
         eval "$__conda_setup"
@@ -62,5 +61,17 @@ if [[ $(uname -s) == 'Darwin' ]]; then
         fi
     fi
     unset __conda_setup
-    # <<< conda initialize <<<
+else
+    __conda_setup="$('/opt/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+    if [ $? -eq 0 ]; then
+        eval "$__conda_setup"
+    else
+        if [ -f "/opt/miniconda3/etc/profile.d/conda.sh" ]; then
+            . "/opt/miniconda3/etc/profile.d/conda.sh"
+        else
+            export PATH="/opt/miniconda3/bin:$PATH"
+        fi
+    fi
+    unset __conda_setup
 fi
+# <<< conda initialize <<<
