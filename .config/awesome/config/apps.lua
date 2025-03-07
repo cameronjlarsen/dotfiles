e738882274471a84f37851ca7223c33448ea0170
Cameron Larsen <camjlarsen@gmail.com>
2025-03-06 22:40:28 -0800

    feat(awesome): add zen browswer as default

--- .config/awesome/config/apps.lua
+++ .config/awesome/config/apps.lua
@@ -3,7 +3,7 @@ apps = {
     terminal     = "wezterm",
     tdrop        = "tdrop -am -w 60% -h 50% -y 25% -x 20% --name 'tdropdropdown' wezterm start --always-new-process",
     text_editor  = "wezterm start nvim",
-    web_browser  = "firefox",
+    web_browser  = "zen-browser",
     file_manager = "thunar",
     launcher     = "rofi -show drun ",
 }
