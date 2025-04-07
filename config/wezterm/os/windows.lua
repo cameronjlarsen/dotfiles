b6a122ea3443981473fd88f58b74f59fa4e4c71a
Cameron Larsen <clarsen@absnetwork.com>
2025-04-07 16:28:44 -0700

    fix(wezterm): incorrect default prog syntax

--- config/wezterm/os/windows.lua
+++ config/wezterm/os/windows.lua
@@ -1,6 +1,6 @@
 return function(config)
     config.term = "xterm-256color"
-    config.default_prog = { "pwsh.exe, -nologo" }
+    config.default_prog = { "pwsh.exe", "-nologo" }
     config.launch_menu = {}
     table.insert(config.launch_menu, { label = "pwsh", args = { "pwsh.exe", "-nologo" } })
     config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
