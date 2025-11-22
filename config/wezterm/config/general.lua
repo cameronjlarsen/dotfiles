2ba82eef2ec6b77629e641e57067501e6bc90f60
Cameron Larsen <camjlarsen@gmail.com>
2025-11-22 00:38:13 -0800

    feat(wezterm): enable kitty graphics

--- config/wezterm/config/general.lua
+++ config/wezterm/config/general.lua
@@ -5,6 +5,8 @@ return function(config)
     -- General settings
     config.automatically_reload_config = true
     config.audible_bell = "Disabled"
+    config.enable_kitty_keyboard = true
+    config.enable_kitty_graphics = true
 
     return config
 end
