be7a2df9cbc4251feecf5729ea1c5c482154b1ac
Cameron Larsen <camjlarsen@gmail.com>
2026-01-10 00:08:16 -0800

    feat(wezterm): migrate keybindings to tmux-style with CTRL+a prefix
    - Replace ALT-based keybindings with leader-key (CTRL+a) tmux-style bindings
    - Disable default wezterm keybindings to prevent conflicts
    - Add 14 recommended utility keybindings:
      - Search: CTRL+a /
      - Quick select: CTRL+a Space
      - Tab management: CTRL+a W/</>
      - Utilities: reload config (r), debug (d), command palette (:), char picker (u)
      - Spawn window: CTRL+a N
      - Reset font: CTRL+a 0
      - Page scroll (no prefix): Shift+PageUp/PageDown
      - Paste primary selection: Shift+Insert
    - Preserve vim/nvim pane navigation integration

--- config/wezterm/config/keybinds.lua
+++ config/wezterm/config/keybinds.lua
@@ -2,119 +2,141 @@ local wezterm = require("wezterm")
 local catppuccin = require("colors.catppuccin")
 
 return function(config)
-    local colors = catppuccin.colors
+	local colors = catppuccin.colors
 
-    config.disable_default_key_bindings = false
-    config.keys = {
-        {
-            mods = "ALT",
-            key = [[\]],
-            action = wezterm.action({
-                SplitHorizontal = { domain = "CurrentPaneDomain" },
-            }),
-        },
-        {
-            mods = "ALT|SHIFT",
-            key = [[|]],
-            action = wezterm.action.SplitPane({
-                top_level = true,
-                direction = "Right",
-                size = { Percent = 50 },
-            }),
-        },
-        {
-            mods = "ALT",
-            key = [[-]],
-            action = wezterm.action({
-                SplitVertical = { domain = "CurrentPaneDomain" },
-            }),
-        },
-        {
-            mods = "ALT|SHIFT",
-            key = [[_]],
-            action = wezterm.action.SplitPane({
-                top_level = true,
-                direction = "Down",
-                size = { Percent = 50 },
-            }),
-        },
-        { key = "t",          mods = "CTRL|SHIFT", action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }), },
-        { key = "w",          mods = "CTRL|SHIFT", action = wezterm.action({ CloseCurrentTab = { confirm = false } }), },
-        { key = "q",          mods = "CTRL|SHIFT", action = wezterm.action.CloseCurrentPane({ confirm = false }) },
-        { key = "z",          mods = "ALT",        action = wezterm.action.TogglePaneZoomState },
-        { key = "F11",        mods = "",           action = wezterm.action.ToggleFullScreen },
-        { key = "h",          mods = "ALT|SHIFT",  action = wezterm.action.AdjustPaneSize({ "Left", 3 }) },
-        { key = "j",          mods = "ALT|SHIFT",  action = wezterm.action.AdjustPaneSize({ "Down", 3 }) },
-        { key = "k",          mods = "ALT|SHIFT",  action = wezterm.action.AdjustPaneSize({ "Up", 3 }) },
-        { key = "l",          mods = "ALT|SHIFT",  action = wezterm.action.AdjustPaneSize({ "Right", 3 }) },
-        { key = "LeftArrow",  mods = "ALT|SHIFT",  action = wezterm.action.AdjustPaneSize({ "Left", 3 }) },
-        { key = "DownArrow",  mods = "ALT|SHIFT",  action = wezterm.action.AdjustPaneSize({ "Down", 3 }) },
-        { key = "UpArrow",    mods = "ALT|SHIFT",  action = wezterm.action.AdjustPaneSize({ "Up", 3 }) },
-        { key = "RightArrow", mods = "ALT|SHIFT",  action = wezterm.action.AdjustPaneSize({ "Right", 3 }) },
-        { key = "h",          mods = "ALT",        action = wezterm.action.EmitEvent("ActivatePaneDirection-left") },
-        { key = "j",          mods = "ALT",        action = wezterm.action.EmitEvent("ActivatePaneDirection-down") },
-        { key = "k",          mods = "ALT",        action = wezterm.action.EmitEvent("ActivatePaneDirection-up") },
-        { key = "l",          mods = "ALT",        action = wezterm.action.EmitEvent("ActivatePaneDirection-right") },
-        { key = "LeftArrow",  mods = "CTRL|SHIFT", action = wezterm.action.EmitEvent("ActivatePaneDirection-left") },
-        { key = "DownArrow",  mods = "CTRL|SHIFT", action = wezterm.action.EmitEvent("ActivatePaneDirection-down") },
-        { key = "UpArrow",    mods = "CTRL|SHIFT", action = wezterm.action.EmitEvent("ActivatePaneDirection-up") },
-        { key = "RightArrow", mods = "CTRL|SHIFT", action = wezterm.action.EmitEvent("ActivatePaneDirection-right") },
-        { key = "[",          mods = "ALT",        action = wezterm.action({ ActivateTabRelative = -1 }) },
-        { key = "]",          mods = "ALT",        action = wezterm.action({ ActivateTabRelative = 1 }) },
-        { key = "{",          mods = "SHIFT|ALT",  action = wezterm.action.MoveTabRelative(-1) },
-        { key = "}",          mods = "SHIFT|ALT",  action = wezterm.action.MoveTabRelative(1) },
-        { key = "v",          mods = "ALT",        action = wezterm.action.ActivateCopyMode },
-        { key = "c",          mods = "CTRL|SHIFT", action = wezterm.action({ CopyTo = "Clipboard" }) },
-        { key = "v",          mods = "CTRL|SHIFT", action = wezterm.action({ PasteFrom = "Clipboard" }) },
-        { key = "=",          mods = "CTRL",       action = wezterm.action.IncreaseFontSize },
-        { key = "-",          mods = "CTRL",       action = wezterm.action.DecreaseFontSize },
-        -- Tab activation shortcuts
-        { key = "1",          mods = "CTRL|SHIFT", action = wezterm.action({ ActivateTab = 1 }) },
-        { key = "2",          mods = "CTRL|SHIFT", action = wezterm.action({ ActivateTab = 2 }) },
-        { key = "3",          mods = "CTRL|SHIFT", action = wezterm.action({ ActivateTab = 3 }) },
-        { key = "4",          mods = "CTRL|SHIFT", action = wezterm.action({ ActivateTab = 4 }) },
-        { key = "5",          mods = "CTRL|SHIFT", action = wezterm.action({ ActivateTab = 5 }) },
-        { key = "6",          mods = "CTRL|SHIFT", action = wezterm.action({ ActivateTab = 6 }) },
-        { key = "7",          mods = "CTRL|SHIFT", action = wezterm.action({ ActivateTab = 7 }) },
-        { key = "8",          mods = "CTRL|SHIFT", action = wezterm.action({ ActivateTab = 8 }) },
-        { key = "9",          mods = "CTRL|SHIFT", action = wezterm.action({ ActivateTab = 9 }) },
-        { key = "Enter",      mods = "ALT",        action = wezterm.action.DisableDefaultAssignment },
+	-- Set leader key to CTRL+a (tmux-style prefix)
+	config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
 
-        -- Workspace management
-        {
-            key = 'i',
-            mods = 'CTRL|SHIFT',
-            action = wezterm.action.PromptInputLine({
-                description = wezterm.format({
-                    { Attribute = { Intensity = 'Bold' } },
-                    { Foreground = { Color = colors.pink } },
-                    { Text = 'Enter name for new workspace' },
-                }),
-                action = wezterm.action_callback(function(window, pane, line)
-                    if line then
-                        window:perform_action(
-                            wezterm.action.SwitchToWorkspace({
-                                name = line,
-                            }),
-                            pane
-                        )
-                    end
-                end),
-            }),
-        },
-        -- Workspace launcher with fuzzy finder
-        {
-            key = "w",
-            mods = "ALT|SHIFT",
-            action = wezterm.action.ShowLauncherArgs({
-                flags = "FUZZY|WORKSPACES",
-            }),
-        },
-        -- Workspace navigation
-        { key = "n", mods = "CTRL", action = wezterm.action.SwitchWorkspaceRelative(1) },
-        { key = "p", mods = "CTRL", action = wezterm.action.SwitchWorkspaceRelative(-1) },
-    }
+	-- Disable default wezterm keybindings to avoid conflicts
+	config.disable_default_key_bindings = true
+	config.keys = {
+		-- Pane splits (tmux-style)
+		{
+			key = [[|]],
+			mods = "LEADER|SHIFT",
+			action = wezterm.action.SplitPane({
+				direction = "Right",
+				size = { Percent = 50 },
+			}),
+		},
+		{
+			key = [[-]],
+			mods = "LEADER",
+			action = wezterm.action.SplitPane({
+				direction = "Down",
+				size = { Percent = 50 },
+			}),
+		},
 
-    return config
+		-- Pane navigation (tmux-style with nvim integration)
+		{ key = "h", mods = "LEADER", action = wezterm.action.EmitEvent("ActivatePaneDirection-left") },
+		{ key = "j", mods = "LEADER", action = wezterm.action.EmitEvent("ActivatePaneDirection-down") },
+		{ key = "k", mods = "LEADER", action = wezterm.action.EmitEvent("ActivatePaneDirection-up") },
+		{ key = "l", mods = "LEADER", action = wezterm.action.EmitEvent("ActivatePaneDirection-right") },
+
+		-- Pane resize (tmux-style with shift)
+		{ key = "H", mods = "LEADER|SHIFT", action = wezterm.action.AdjustPaneSize({ "Left", 3 }) },
+		{ key = "J", mods = "LEADER|SHIFT", action = wezterm.action.AdjustPaneSize({ "Down", 3 }) },
+		{ key = "K", mods = "LEADER|SHIFT", action = wezterm.action.AdjustPaneSize({ "Up", 3 }) },
+		{ key = "L", mods = "LEADER|SHIFT", action = wezterm.action.AdjustPaneSize({ "Right", 3 }) },
+
+		-- Pane management
+		{ key = "z", mods = "LEADER", action = wezterm.action.TogglePaneZoomState },
+		{ key = "x", mods = "LEADER", action = wezterm.action.CloseCurrentPane({ confirm = false }) },
+
+		-- Tab/Window management (tmux-style)
+		{ key = "c", mods = "LEADER", action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
+		{ key = "n", mods = "LEADER", action = wezterm.action({ ActivateTabRelative = 1 }) },
+		{ key = "p", mods = "LEADER", action = wezterm.action({ ActivateTabRelative = -1 }) },
+
+		-- Tab activation by number (1-9)
+		{ key = "1", mods = "LEADER", action = wezterm.action({ ActivateTab = 0 }) },
+		{ key = "2", mods = "LEADER", action = wezterm.action({ ActivateTab = 1 }) },
+		{ key = "3", mods = "LEADER", action = wezterm.action({ ActivateTab = 2 }) },
+		{ key = "4", mods = "LEADER", action = wezterm.action({ ActivateTab = 3 }) },
+		{ key = "5", mods = "LEADER", action = wezterm.action({ ActivateTab = 4 }) },
+		{ key = "6", mods = "LEADER", action = wezterm.action({ ActivateTab = 5 }) },
+		{ key = "7", mods = "LEADER", action = wezterm.action({ ActivateTab = 6 }) },
+		{ key = "8", mods = "LEADER", action = wezterm.action({ ActivateTab = 7 }) },
+		{ key = "9", mods = "LEADER", action = wezterm.action({ ActivateTab = 8 }) },
+
+		-- Copy mode (tmux-style)
+		{ key = "[", mods = "LEADER", action = wezterm.action.ActivateCopyMode },
+
+		-- Workspace management (tmux-style)
+		{ key = "(", mods = "LEADER|SHIFT", action = wezterm.action.SwitchWorkspaceRelative(-1) },
+		{ key = ")", mods = "LEADER|SHIFT", action = wezterm.action.SwitchWorkspaceRelative(1) },
+
+		-- Workspace switcher (fuzzy finder)
+		{
+			key = "s",
+			mods = "LEADER",
+			action = wezterm.action.ShowLauncherArgs({
+				flags = "FUZZY|WORKSPACES",
+			}),
+		},
+
+		-- Create/rename workspace
+		{
+			key = "$",
+			mods = "LEADER|SHIFT",
+			action = wezterm.action.PromptInputLine({
+				description = wezterm.format({
+					{ Attribute = { Intensity = "Bold" } },
+					{ Foreground = { Color = colors.pink } },
+					{ Text = "Enter name for workspace" },
+				}),
+				action = wezterm.action_callback(function(window, pane, line)
+					if line then
+						window:perform_action(
+							wezterm.action.SwitchToWorkspace({
+								name = line,
+							}),
+							pane
+						)
+					end
+				end),
+			}),
+		},
+
+		-- Send literal CTRL+a to application (press leader twice)
+		{ key = "a", mods = "LEADER", action = wezterm.action.SendKey({ key = "a", mods = "CTRL" }) },
+
+		-- Standard clipboard operations (unchanged, no prefix needed)
+		{ key = "c", mods = "CTRL|SHIFT", action = wezterm.action({ CopyTo = "Clipboard" }) },
+		{ key = "v", mods = "CTRL|SHIFT", action = wezterm.action({ PasteFrom = "Clipboard" }) },
+
+		-- Font size adjustments (unchanged, no prefix needed)
+		{ key = "=", mods = "CTRL", action = wezterm.action.IncreaseFontSize },
+		{ key = "-", mods = "CTRL", action = wezterm.action.DecreaseFontSize },
+
+		-- Fullscreen toggle (unchanged, no prefix needed)
+		{ key = "F11", mods = "", action = wezterm.action.ToggleFullScreen },
+
+		-- Search and selection (with prefix)
+		{ key = "/", mods = "LEADER", action = wezterm.action.Search({ CaseInSensitiveString = "" }) },
+		{ key = "Space", mods = "LEADER", action = wezterm.action.QuickSelect },
+
+		-- Tab management additions
+		{ key = "q", mods = "LEADER", action = wezterm.action.CloseCurrentTab({ confirm = false }) },
+		{ key = "<", mods = "LEADER|SHIFT", action = wezterm.action.MoveTabRelative(-1) },
+		{ key = ">", mods = "LEADER|SHIFT", action = wezterm.action.MoveTabRelative(1) },
+
+		-- Utilities (with prefix)
+		{ key = "r", mods = "LEADER", action = wezterm.action.ReloadConfiguration },
+		{ key = "d", mods = "LEADER", action = wezterm.action.ShowDebugOverlay },
+		{ key = ":", mods = "LEADER|SHIFT", action = wezterm.action.ActivateCommandPalette },
+		{ key = "u", mods = "LEADER", action = wezterm.action.CharSelect },
+		{ key = "N", mods = "LEADER|SHIFT", action = wezterm.action.SpawnWindow },
+		{ key = "0", mods = "LEADER", action = wezterm.action.ResetFontSize },
+
+		-- Scrolling by page (no prefix needed)
+		{ key = "PageUp", mods = "SHIFT", action = wezterm.action.ScrollByPage(-1) },
+		{ key = "PageDown", mods = "SHIFT", action = wezterm.action.ScrollByPage(1) },
+
+		-- Paste primary selection (X11)
+		{ key = "Insert", mods = "SHIFT", action = wezterm.action.PasteFrom("PrimarySelection") },
+	}
+
+	return config
 end
-
